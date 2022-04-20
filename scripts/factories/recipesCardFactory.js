d491cc801bcbe9ecd6d1f928dcca39539a1cc806
joncoder <joncoder.kamite@gmail.com>
2022-04-21 01:44:35 +0200

    créé des cartes de recettes de cuisine

--- scripts/factories/recipesCardFactory.js
+++ scripts/factories/recipesCardFactory.js
@@ -3,24 +3,35 @@ class RecipiesCardFactory {
     constructor(recipesCard){
         this._recipesCard = recipesCard;
     }
-
+//function RecipiesCardFactory(recipesCard){
+    
     get randomItem(){
-        const _Items = [
+    //function randomItem() {
+        const Items = [
             'https://img.cuisineaz.com/610x610/2016-02-09/i8552-curry-rouge-thai-aux-crevettes.jpeg',
             'https://i.pinimg.com/originals/83/c8/51/83c851d0cdd992995c077a1a85577b31.jpg',
             'https://img.mesrecettesfaciles.fr/2020-12/cocktail-de-noel-1.jpg',
-            'https://i.pinimg.com/736x/e2/33/06/e23306b49a85da8ea8fdd53a574a26dc.jpg'
+            'https://i.pinimg.com/736x/e2/33/06/e23306b49a85da8ea8fdd53a574a26dc.jpg',
+            'https://recettes-light.fr/wp-content/uploads/2010/06/Fotolia_10194783_XS.jpg',
+            'http://www.cuisineetsentiments.com/images/recette-aubergine_9.jpg',
+            'https://www.recette-de-salade.com/wp-content/uploads/2012/01/salade-au-chevre-chaud.jpg',
+            'https://youcookcuisine.com/wp-content/uploads/2019/01/piemontaise-revisitee-4-400x400.jpg',
+            'https://media.houra.fr/images/widget/recette/gd_recette_filet_boeuf.jpg',
+            'https://www.toques2cuisine.com/wp-content/uploads/2012/11/CANETTE-AU-CIDRE-089.jpg',
+            'http://www.cuisineetsentiments.com/images/recette-cuisse-de-poulet_2.jpg'
         ];
-        return _Items[Math.floor(Math.random()*_Items.length)]
+        return Items[Math.floor(Math.random()*Items.length)];
     }
 
     get buildCard(){
+    //function buildCard() {
         const article = document.createElement('article');
         article.classList.add('recipe_card','card');
         const figure = document.createElement('figure');
         const img = document.createElement('img');
         img.classList.add('card-img-top');
         img.src = this.randomItem;
+        //img.src = randomItem();
         figure.appendChild(img);
 
         const divRecipe = document.createElement('div');
@@ -31,10 +42,12 @@ class RecipiesCardFactory {
         const h4Name = document.createElement('h4');
         h4Name.classList.add('name','card-title');
         h4Name.textContent = this._recipesCard['name'];
+        //h4Name.textContent = recipesCard['name'];
         const pIngredients = document.createElement('p');
         pIngredients.classList.add('ingredients', 'card-text');
         
         this._recipesCard['ingredients'].forEach(ingredient =>{
+        //recipesCard['ingredients'].forEach(ingredient =>{
             const br = document.createElement('br');
             const strong = document.createElement('strong');
             strong.textContent = ingredient['ingredient'];
@@ -64,11 +77,30 @@ class RecipiesCardFactory {
         const minutes = document.createElement('span');
         minutes.classList.add('minutes');
         minutes.textContent = this._recipesCard['time'] + ' min';
+        //minutes.textContent = recipesCard['time'] + ' min';
         h4Time.appendChild(iconTime);
         h4Time.appendChild(minutes);
         const pDesc = document.createElement('p');
         pDesc.classList.add('description', 'card-text');
         pDesc.textContent = this._recipesCard['description'];
+        //pDesc.textContent = recipesCard['description'];
+
+        const appliance = document.createElement('div');
+        appliance.classList.add('appliance', 'hide-elem');
+        appliance.textContent = this._recipesCard['appliance'];
+
+        const ustensils = document.createElement('div');
+        ustensils.classList.add('ustensils', 'hide-elem');
+        this._recipesCard['ustensils'].forEach(ustensil => {
+            const spanUstensil = document.createElement('span');
+            spanUstensil.textContent = ustensil;
+            ustensils.appendChild(spanUstensil);
+        })
+
+        const utilApp = document.createElement('div');
+        utilApp.classList.add('utilApp');
+        utilApp.appendChild(appliance);
+        utilApp.appendChild(ustensils);
 
         divTimeDesc.appendChild(h4Time);
         divTimeDesc.appendChild(pDesc);
@@ -78,7 +110,10 @@ class RecipiesCardFactory {
 
         article.appendChild(figure);
         article.appendChild(divRecipe);
+        article.appendChild(utilApp);
 
         return (article);
     }
+
+    //return {buildCard}
 }
