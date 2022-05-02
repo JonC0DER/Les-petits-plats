e42cb26746ada144888b4243195e7d81068d3152
joncoder <joncoder.kamite@gmail.com>
2022-05-02 14:52:50 +0200

    trie alphabetique des datas recepies

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const content = document.querySelector('main.gallerie_recipies');
 const infoPage = document.querySelector('.page_info');
 const arrayRecipies = new Array();
+let recipiesDatas;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -8,8 +9,14 @@ function sleep(ms) {
 
 function getRecipes() {
     const datas = new GetData('initFirst');
-    const recipiesDatas = datas.recipes;
-    
+    recipiesDatasUnsorted = datas.recipes;
+    recipiesDatas = recipiesDatasUnsorted.sort(
+        (a, b) => a.name.localeCompare(
+            b.name, 'fr', 
+            {sensitivity: 'base', ignorePonctuation:true}
+        )
+    );
+
     return initRecipes(recipiesDatas);
 }
 
@@ -27,12 +34,13 @@ function initRecipes(recipiesDatas) {
 loadedRecipes = false;
 setTimeout(function() {
     getRecipes();
-    console.log('first')
+    btns.setValuesInArray(arrayRecipies);
+    //console.log('first')
     update()
 },1000)
 document.addEventListener('DOMContentLoaded', function(){
     getRecipes();
-    console.log('second')
+    //console.log('second')
 })
 
 const pagination = new Pagination(arrayRecipies);    
@@ -40,7 +48,7 @@ function update() {
     pagination.displayRecipies();
     content.innerHTML = pagination.pageContent;
     infoPage.textContent = pagination.pageInfo;
-    console.log('third')
+    //console.log('third')
 }
 
 function firstPageRecipes() {
@@ -62,64 +70,3 @@ function lastPageRecipes() {
    pagination.lastPageRecipes()
    update() 
 }
-
-
-const btnsColoed = document.querySelectorAll('div.list_nav');
-const spanTime = 200;
-const btnTime = 200;
-function closeAllcoloredBtn() {
-    let count = 0;
-    btnsColoed.forEach(async btn => {
-        if (btn.classList.contains('deploy')) {
-            btn.children[1].childNodes.forEach(span => {
-                span.classList.add('reversetrans');
-            })
-            await sleep(spanTime);
-            btn.children[1].childNodes.forEach(span => {
-                span.classList.remove('reversetrans');
-            })
-            await sleep(spanTime);
-            btn.classList.add('colorbtn-anim-close');
-            btn.children[1].classList.add('colorbtn-anim-close');
-            btn.classList.remove('deploy');
-            await sleep(btnTime);
-            btn.children[1].classList.remove('colorbtn-anim-close');
-            btn.classList.remove('colorbtn-anim-close');
-            btn.children[1].classList.remove('deploy-content');
-            btn.classList.remove('deploy_radius');
-            btn.children[1].classList.add('hide-elem');
-        }
-        count ++;
-    })
-    return btnsColoed.length === count;
-}
-
-btnsColoed.forEach(btn => {
-    btn.addEventListener('click', async function() {
-        closeAllcoloredBtn();
-        if (!btn.classList.contains('deploy')) { 
-            // OPEN !
-            btn.children[1].classList.remove('hide-elem');
-            btn.children[1].classList.add('colorbtn-anim-open');
-            btn.classList.add('colorbtn-anim-open');
-            btn.classList.add('deploy');
-            btn.classList.add('deploy_radius');
-            btn.children[1].classList.add('deploy-content');
-            await sleep(btnTime);
-            btn.children[1].childNodes.forEach(span => {
-                span.classList.add('animtrans');
-            })
-            await sleep(spanTime);
-            btn.children[1].childNodes.forEach(span => {
-                span.classList.remove('animtrans');
-            })
-            await sleep(spanTime);
-            btn.classList.remove('colorbtn-anim-open');
-            btn.children[1].classList.remove('colorbtn-anim-open');
-        }else{ 
-            // CLOSE !
-            closeAllcoloredBtn();
-        }
-        
-    })
-})
