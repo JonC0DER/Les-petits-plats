e256df04d01faa2a52f5e246e1f68544c397546d
joncoder <joncoder.kamite@gmail.com>
2022-05-25 14:16:04 +0200

    mise à niveau fc initRecipes + ajout array dict & preciseArrayRecipes

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,9 @@ const content = document.querySelector('main.gallerie_recipies');
 const infoPage = document.querySelector('.page_info');
 const arrayRecipies = new Array();
 const btns = coloredBtn();
-let recipiesDatas;
+let recipiesDatas; 
+let preciseArrayRecipies;
+let dict = new Array();
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -28,6 +30,27 @@ function initRecipes(recipiesDatas) {
             arrayRecipies.push(card.buildCard);
         }
     }
+
+    for (let i = 0; i < recipiesDatas.length; i++) {
+        const recipeIndex = recipiesDatas.indexOf(recipiesDatas[i]);
+        const words = recipiesDatas[i]['name'].split(' ');
+        for (let z = 0; z < words.length; z++) {
+            const word = words[z];
+            const matchArray = dict.filter(a => a['name'].match(word));
+            if (dict.length > 0 && matchArray.length > 0) {
+                dict[dict.indexOf(matchArray[0])]['indexes'].push(recipeIndex);
+            }
+            else if (word.length > 2) {
+                const indexes = new Array();
+                indexes.push(recipeIndex);
+                dict.push({name: word, indexes: indexes});
+            }
+        }        
+    }
+    dict.sort((a,b) => a['name'].localeCompare(
+        b['name'], 'fr', 
+        {sensitivity:'base', ignorePonctuation:true}
+    ))
     
     const recipiesDatasLen = recipiesDatas.length;
     for (let i = 0; i < recipiesDatasLen; i++) {
